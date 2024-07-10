8d2947a568adff505646d14f5aa07bd05fd8db02
Phil A <philander90@msn.com>
2024-07-10 20:33:30 +0800

    Update RandomizerUtility.java

--- src/main/java/RandomizerUtility.java
+++ src/main/java/RandomizerUtility.java
@@ -1,9 +1,37 @@
 import java.util.Date;
 
-public class RandomizerUtility {
-	public static void log(String... lines) {
-		for (String line : lines) {
-			System.out.println(new Date() + " " + line);
-		}
-	}
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class RandomizerUtility
+{
+
+  public static void log(String... lines)
+  {
+    for (String line : lines)
+    {
+      System.out.println(new Date() + " " + line);
+    }
+  }
+
+  private static int dialog(int option, String title, String... msgs)
+      throws ClassNotFoundException, InstantiationException, IllegalAccessException,
+      UnsupportedLookAndFeelException
+  {
+    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    return JOptionPane.showConfirmDialog(null, String.join("\n", msgs), title, option);
+  }
+
+  public static int dialogYesNo(String title, String... msgs) throws ClassNotFoundException,
+      InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException
+  {
+    return dialog(JOptionPane.YES_NO_OPTION, title, msgs);
+  }
+
+  public static int dialogError(String title, String... msgs) throws ClassNotFoundException,
+      InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException
+  {
+    return dialog(JOptionPane.ERROR_MESSAGE, title, msgs);
+  }
 }
